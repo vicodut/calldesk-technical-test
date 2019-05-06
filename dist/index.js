544bdd4a1de1149660506bfe1f4f90bbae65bfcb
ysiguman <victord675@gmail.com>
2019-05-06 21:18:34 +0200

    Currency finish convert

--- dist/index.js
+++ dist/index.js
@@ -1,33 +1,30 @@
 const http = require('http');
 const functions = require('firebase-functions');
 
-const currHost = 'http://free.currconv.com/api/v7/convert?q=USD_PHP&apiKey=5a72d29dab3ab35a04c6';
-
-/**
- * callWeatherApi - description
- *
- * @param  {type} city description
- * @param  {type} date description
- * @return {type}      description
- */
+const host = 'http://free.currconv.com/api/v7/convert';
+const key = '5a72d29dab3ab35a04c6';
 
 /**
  * getCurrency - description
  *
- * @param  {type} curr description
- * @return {type}      description
+ * @param  {type} currencyPair description
+ * @param  {type} amount       description
+ * @return {type}              description
  */
-function getCurrency() {
+function getCurrency(currencyPair) {
+  const url = `${host}?q=${currencyPair}&apiKey=${key}`;
+  console.log(url);
   return new Promise((resolve, reject) => {
-    http.get(currHost, res => {
+    http.get(url, res => {
       let body = '';
+
       res.on('data', d => {
         body += d;
       });
 
       res.on('end', () => {
         const response = JSON.parse(body);
-        resolve(response.results.USD_PHP.val);
+        resolve(response.results[currencyPair].val);
       });
 
       res.on('error', error => {
@@ -37,22 +34,37 @@ function getCurrency() {
   });
 }
 
+/**
+ * buildResponse - description
+ *
+ * @param  {type} output: number  description
+ * @param  {type} amount: number  description
+ * @param  {type} currencies: any description
+ * @return {type}                 description
+ */
+function buildResponse(output, amount, currencies) {
+  return `${amount} ${currencies.curr1} is ${(amount * output).toFixed(2)} ${currencies.curr2}`;
+}
+
 exports.helloWorld = functions.https.onRequest((req, res) => {
   let amount = 1;
   let curr1 = '';
   let curr2 = '';
+  let currencyPair = '';
 
-  if (typeof req.body.queryResult.parameters.curr1 === 'object') {
-    ({ amount, curr2 } = req.body.queryResult.parameters);
-    ({ amount } = req.body.queryResult.parameters.curr1);
-    curr1 = req.body.queryResult.parameters.curr1.currency;
+  if (req.body.queryResult.parameters.currency.amount) {
+    ({ curr2 } = req.body.queryResult.parameters);
+    ({ amount } = req.body.queryResult.parameters.currency);
+    curr1 = req.body.queryResult.parameters.currency.currency;
   } else {
     ({ curr1, curr2 } = req.body.queryResult.parameters);
   }
 
-  console.log(amount, curr1, curr2);
-  getCurrency().then(output => {
-    res.json({ fulfillmentText: output });
+  currencyPair = `${curr1}_${curr2}`;
+
+  getCurrency(currencyPair).then(output => {
+    const result = buildResponse(output, amount, { curr1, curr2 });
+    res.json({ fulfillmentText: result });
   }).catch(error => {
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ speech: error, displayText: error }));
--- src/index.js
+++ src/index.js
@@ -3,33 +3,29 @@
 const http = require('http');
 const functions = require('firebase-functions');
 
-const currHost = 'http://free.currconv.com/api/v7/convert?q=USD_PHP&apiKey=5a72d29dab3ab35a04c6';
-
-/**
- * callWeatherApi - description
- *
- * @param  {type} city description
- * @param  {type} date description
- * @return {type}      description
- */
+const host = 'http://free.currconv.com/api/v7/convert';
+const key = '5a72d29dab3ab35a04c6';
 
 /**
  * getCurrency - description
  *
- * @param  {type} curr description
- * @return {type}      description
+ * @param  {type} currencyPair description
+ * @param  {type} amount       description
+ * @return {type}              description
  */
-function getCurrency() {
+function getCurrency(currencyPair) {
+  const url = `${host}?q=${currencyPair}&apiKey=${key}`;
   return new Promise((resolve, reject) => {
-    http.get(currHost, (res) => {
+    http.get(url, (res) => {
       let body = '';
+
       res.on('data', (d) => {
         body += d;
       });
 
       res.on('end', () => {
         const response = JSON.parse(body);
-        resolve(response.results.USD_PHP.val);
+        resolve(response.results[currencyPair].val);
       });
 
       res.on('error', (error) => {
@@ -39,22 +35,38 @@ function getCurrency() {
   });
 }
 
+
+/**
+ * buildResponse - description
+ *
+ * @param  {type} output: number  description
+ * @param  {type} amount: number  description
+ * @param  {type} currencies: any description
+ * @return {type}                 description
+ */
+function buildResponse(output: number, amount: number, currencies: any) {
+  return `${amount} ${currencies.curr1} is ${(amount * output).toFixed(2)} ${currencies.curr2}`;
+}
+
 exports.helloWorld = functions.https.onRequest((req, res) => {
   let amount: number = 1;
   let curr1: string = '';
   let curr2: string = '';
+  let currencyPair: string = '';
 
-  if (typeof req.body.queryResult.parameters.curr1 === 'object') {
-    ({ amount, curr2 } = req.body.queryResult.parameters);
-    ({ amount } = req.body.queryResult.parameters.curr1);
-    curr1 = req.body.queryResult.parameters.curr1.currency;
+  if (req.body.queryResult.parameters.currency.amount) {
+    ({ curr2 } = req.body.queryResult.parameters);
+    ({ amount } = req.body.queryResult.parameters.currency);
+    curr1 = req.body.queryResult.parameters.currency.currency;
   } else {
     ({ curr1, curr2 } = req.body.queryResult.parameters);
   }
 
-  console.log(amount, curr1, curr2);
-  getCurrency().then((output) => {
-    res.json({ fulfillmentText: output });
+  currencyPair = `${curr1}_${curr2}`;
+
+  getCurrency(currencyPair).then((output) => {
+    const result: string = buildResponse(output, amount, { curr1, curr2 });
+    res.json({ fulfillmentText: result });
   }).catch((error) => {
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ speech: error, displayText: error }));
